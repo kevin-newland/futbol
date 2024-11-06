c6cf79970fcd3505c8a2f4820113f7c4773a6c7b
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2024-11-06 23:38:08 +0100

    Add initial StatCalculator class and test file

---
+++ lib/stat_calculator.rb
@@ -0,0 +1,5 @@
+# lib/stat_calculator.rb
+
+class StatCalculator
+  # Methods and logic will go here
+end
---
+++ spec/stat_calculator_spec.rb
@@ -0,0 +1,7 @@
+# spec/stat_calculator_spec.rb
+require 'rspec'
+require_relative '../lib/stat_calculator'
+
+RSpec.describe StatCalculator do
+  # Tests will go here
+end
