99f52ef9b6baaf5a74e060b284bc071cd3b73c05
Dustin Peukert <dpeukert04@gmail.com>
2024-11-06 18:18:02 -0500

    create and initialize GameTeams class

---
+++ lib/game_teams.rb
@@ -0,0 +1,25 @@
+class GameTeams
+  @@instances = []
+
+  attr_reader :game_id,
+              :team_id,
+              :hoa,
+              :result,
+              :head_coach,
+              :goals,
+              :shots,
+              :tackles
+
+  def initialize(game_id, team_id, hoa, result, head_coach, goals, shots, tackles)
+    @game_id = game_id
+    @team_id = team_id
+    @hoa = hoa
+    @result = result
+    @head_coach = head_coach
+    @goals = goals
+    @shots = shots
+    @tackles = tackles
+
+    @@instances << self
+  end
+end
