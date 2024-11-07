66badfb74e30b54dbfc0ee7350b7445f77cee979
Dustin Peukert <dpeukert04@gmail.com>
2024-11-07 17:17:04 -0500

    write #count_of_teams

--- lib/stat_calculator.rb
+++ lib/stat_calculator.rb
@@ -2,10 +2,14 @@ class StatCalculator
   attr_reader :games,
               :teams,
               :game_teams
-              
+
   def initialize(games, teams, game_teams)
     @games = games
     @teams = teams
     @game_teams = game_teams
+  end
+
+  def count_of_teams
+    @teams.count
   end
 end
