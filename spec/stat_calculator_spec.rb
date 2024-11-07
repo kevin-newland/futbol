9c8da44f0a4e526a4e78450bd99ce5dff8e4de3c
Dustin Peukert <dpeukert04@gmail.com>
2024-11-07 17:16:07 -0500

    write #count_of_teams method test

--- spec/stat_calculator_spec.rb
+++ spec/stat_calculator_spec.rb
@@ -9,20 +9,25 @@ RSpec.describe StatCalculator do
     @games = Games.all
     @teams = Teams.all
     @game_teams = GameTeams.all
+    @stat_calculator = StatCalculator.new(@games, @teams, @game_teams)
   end
 
   describe '#initialize' do
     it 'exists' do
-      stat_calculator = StatCalculator.new(@games, @teams, @game_teams)
-      expect(stat_calculator).to be_an_instance_of(StatCalculator)
+      expect(@stat_calculator).to be_an_instance_of(StatCalculator)
     end
 
     it 'has attributes' do
-      stat_calculator = StatCalculator.new(@games, @teams, @game_teams)
+      expect(@stat_calculator.games).to eq(Games.all)
+      expect(@stat_calculator.teams).to eq(Teams.all)
+      expect(@stat_calculator.game_teams).to eq(GameTeams.all)
+    end
+  end
 
-      expect(stat_calculator.games).to eq(Games.all)
-      expect(stat_calculator.teams).to eq(Teams.all)
-      expect(stat_calculator.game_teams).to eq(GameTeams.all)
+  describe '#count_of_teams' do
+    it 'returns the number of teams' do
+      expect(@stat_calculator.count_of_teams).to eq(32)
+      expect(Teams.all.count).to eq(32)
     end
   end
 end
