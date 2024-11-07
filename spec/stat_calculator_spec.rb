55ad7997cdcdf02a93b56b24fb2f6d53bf460a17
Dustin Peukert <dpeukert04@gmail.com>
2024-11-07 16:58:31 -0500

    add arguments to initialize as well as new test

--- spec/stat_calculator_spec.rb
+++ spec/stat_calculator_spec.rb
@@ -1,10 +1,28 @@
 require_relative 'spec_helper'
 
 RSpec.describe StatCalculator do
+  before(:all) do
+    Games.load_csv('./data/games.csv')
+    Teams.load_csv('./data/teams.csv')
+    GameTeams.from_csv('./data/game_teams.csv')
+
+    @games = Games.all
+    @teams = Teams.all
+    @game_teams = GameTeams.all
+  end
+
   describe '#initialize' do
     it 'exists' do
-      stat_calculator = StatCalculator.new
+      stat_calculator = StatCalculator.new(@games, @teams, @game_teams)
       expect(stat_calculator).to be_an_instance_of(StatCalculator)
+    end
+
+    it 'has attributes' do
+      stat_calculator = StatCalculator.new(@games, @teams, @game_teams)
+
+      expect(stat_calculator.games).to eq(Games.all)
+      expect(stat_calculator.teams).to eq(Teams.all)
+      expect(stat_calculator.game_teams).to eq(GameTeams.all)
     end
   end
 end
