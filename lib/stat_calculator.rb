c3398ffa81f4a57c5e18a205f887ac0301ccf162
Kevin Newland <kevin.newland@yahoo.com>
2024-11-07 17:21:23 -0600

    initialize and write method for average goal per game

--- lib/stat_calculator.rb
+++ lib/stat_calculator.rb
@@ -1,5 +1,32 @@
-# lib/stat_calculator.rb
-
 class StatCalculator
-  # Additional methods for calculations will go here
+  attr_reader :games,
+              :teams,
+              :game_teams
+
+  def initialize(games, teams, game_teams)
+    @games = games
+    @teams = teams
+    @game_teams = game_teams
+  end
+  
+  def average_goals_per_game
+    away_goals = 0
+    home_goals = 0
+    total_games = 0
+
+    @games.each do |game|
+      away_goals += game.away_goals
+      home_goals += game.home_goals
+      total_games += 1
+    end
+
+    total_goals = away_goals + home_goals
+
+    average_goals = total_goals.to_f/total_games.to_f  #when dividing both need to be a float in order to work
+    average_goals.round(2)
+  end
+
+  # def average_goals_per_season
+    
+  # end
 end
