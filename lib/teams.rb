793b43887857b2952bb27cdaf62031eb275fed30
Kevin Newland <kevin.newland@yahoo.com>
2024-11-06 19:04:51 -0600

    add class variable as well as method self.all

--- lib/teams.rb
+++ lib/teams.rb
@@ -2,6 +2,7 @@ class Teams
   attr_reader :team_id, 
               :teamName
 
+   @@instances = []
 
   def initialize(team_id, teamName)
     @team_id = team_id
@@ -9,11 +10,14 @@ class Teams
   end
 
   def self.load_csv(file_path)
-    teams = []
     CSV.foreach('./spec/fixtures/fixture_teams.csv', headers: true, header_converters: :symbol) do |row|
-      team = Teams.new(row[:team_id].to_i, row[:teamName].to_s)
-      teams << team
+      team = Teams.new(row[:team_id].to_i, row[:teamname])
+      @@instances << team
     end
-    teams
+    @@instances
+  end
+
+  def self.all
+    @@instances
   end
 end
