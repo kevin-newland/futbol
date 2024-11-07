8e7e44accdb4030f1911ad6b78a820a3e1620448
Kristin Weiland <kristinweiland@gmail.com>
2024-11-07 15:00:20 -0800

    Add lowest_total_score and highest_total_score methods and respective tests.

--- spec/stat_calculator_spec.rb
+++ spec/stat_calculator_spec.rb
@@ -1,10 +1,37 @@
-# spec/stat_calculator_spec.rb
-require 'rspec'
-require_relative '../lib/stat_calculator'
+require_relative 'spec_helper'
 
 RSpec.describe StatCalculator do
-  it 'exists' do
-    stat_calculator = StatCalculator.new
-    expect(stat_calculator).to be_an_instance_of(StatCalculator)
+  before(:all) do
+    Games.load_csv('./data/games.csv')
+    Teams.load_csv('./data/teams.csv')
+    GameTeams.from_csv('./data/game_teams.csv')
+  
+    @games = Games.all
+    @teams = Teams.all
+    @game_teams = GameTeams.all
+    @stat_calculator = StatCalculator.new(@games, @teams, @game_teams)
   end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@stat_calculator).to be_an_instance_of(StatCalculator)
+    end
+
+    it 'has attributes' do
+      expect(@stat_calculator.games).to eq(Games.all)
+      expect(@stat_calculator.teams).to eq(Teams.all)
+      expect(@stat_calculator.game_teams).to eq(GameTeams.all)
+    end
+  end
+
+  describe 'score stastistics' do
+    it 'returns the highest total score' do
+      expect(@stat_calculator.highest_total_score).to eq(11)
+    end
+
+    it 'returns the lowest total score' do
+      expect(@stat_calculator.lowest_total_score).to eq(0)  
+    end
+  end
+
 end
