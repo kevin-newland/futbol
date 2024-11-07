642a3e700d6c816dc4e67d6a181a317b00c5ac4b
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2024-11-07 23:09:35 +0100

    Add percentage_visitor_wins method with test

--- lib/stat_calculator.rb
+++ lib/stat_calculator.rb
@@ -1,5 +1,14 @@
 # lib/stat_calculator.rb
-
 class StatCalculator
-  # Additional methods for calculations will go here
+  def percentage_home_wins(games)
+    total_games = games.size
+    home_wins = games.count { |game| game[:home_goals].to_i > game[:away_goals].to_i }
+    ((home_wins.to_f / total_games) * 100).round(2)
+  end
+
+  def percentage_visitor_wins(games)
+    total_games = games.size
+    visitor_wins = games.count { |game| game[:away_goals].to_i > game[:home_goals].to_i }
+    ((visitor_wins.to_f / total_games) * 100).round(2)
+  end
 end
--- spec/stat_calculator_spec.rb
+++ spec/stat_calculator_spec.rb
@@ -1,9 +1,25 @@
 # spec/stat_calculator_spec.rb
-require_relative 'spec_helper'
+require 'spec_helper'
 
 RSpec.describe StatCalculator do
+  before(:all) do
+    @games_data = [
+      { home_goals: '3', away_goals: '2' }, # Home win
+      { home_goals: '1', away_goals: '2' }, # Visitor win
+      { home_goals: '4', away_goals: '1' }  # Home win
+    ]
+    @stat_calculator = StatCalculator.new
+  end
+
   it 'exists' do
-    stat_calculator = StatCalculator.new
-    expect(stat_calculator).to be_an_instance_of(StatCalculator)
+    expect(@stat_calculator).to be_an_instance_of(StatCalculator)
+  end
+
+  it 'calculates percentage_home_wins' do
+    expect(@stat_calculator.percentage_home_wins(@games_data)).to eq(66.67) # 2 out of 3 games are home wins
+  end
+
+  it 'calculates percentage_visitor_wins' do
+    expect(@stat_calculator.percentage_visitor_wins(@games_data)).to eq(33.33) # 1 out of 3 games are visitor wins
   end
 end
