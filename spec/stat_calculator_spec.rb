6bd9a1e8b70214a4e41475cc2b532ad16441b82e
Kevin Newland <kevin.newland@yahoo.com>
2024-11-07 17:20:33 -0600

    write before all setup and tests  for Average goal per game

--- spec/stat_calculator_spec.rb
+++ spec/stat_calculator_spec.rb
@@ -1,10 +1,33 @@
-# spec/stat_calculator_spec.rb
-require 'rspec'
-require_relative '../lib/stat_calculator'
+require_relative 'spec_helper'
 
 RSpec.describe StatCalculator do
-  it 'exists' do
-    stat_calculator = StatCalculator.new
-    expect(stat_calculator).to be_an_instance_of(StatCalculator)
+  before(:all) do
+    Games.load_csv('./data/games.csv')
+    GameTeams.from_csv('./data/game_teams.csv')
+    Teams.load_csv('./data/teams.csv')
+
+    @games = Games.all
+    @teams = Teams.all
+    @game_teams = Games.all
+    @stat_calculator = StatCalculator.new(@games, @teams, @game_teams)
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@stat_calculator).to be_an_instance_of(StatCalculator)
+    end
+
+    it 'has attributes' do
+      expect(@stat_calculator.games).to eq(Games.all)
+      expect(@stat_calculator.teams).to eq(Teams.all)
+      expect(@stat_calculator.game_teams).to eq(Games.all)
+    end
+  end
+
+  describe '#average_goals_per_game' do
+    it "#average_goals_per_game" do
+      expect(@stat_calculator.average_goals_per_game).to eq(4.22)
+    end
   end
 end
+ 
