1df30eb28ad444a2143fce4b8b2f33082e410005
Dustin Peukert <dpeukert04@gmail.com>
2024-11-06 19:24:20 -0500

    write #from_csv class method

--- lib/game_teams.rb
+++ lib/game_teams.rb
@@ -23,7 +23,29 @@ class GameTeams
     @@instances << self
   end
 
+  def self.from_csv(game_teams_path)
+    CSV.foreach(game_teams_path, headers: true, header_converters: :symbol) do |row|
+      self.new(
+        row[:game_id],
+        row[:team_id],
+        row[:hoa],
+        row[:result],
+        row[:head_coach],
+        row[:goals].to_i,
+        row[:shots].to_i,
+        row[:tackles].to_i
+      )
+    end
+  end
+
   def self.all
     @@instances
+  end
+
+  # This method is for TESTING ONLY
+  # Needed a way to reset @instances between the tests as class
+  # variables persist throughout a file
+  def self.reset
+    @@instances.clear
   end
 end
