0a7367921bf30062af697daa0ce6f8578f261b48
Kevin Newland <kevin.newland@yahoo.com>
2024-11-07 19:30:19 -0600

    write tests for average goals by season

--- spec/stat_calculator_spec.rb
+++ spec/stat_calculator_spec.rb
@@ -29,5 +29,19 @@ RSpec.describe StatCalculator do
       expect(@stat_calculator.average_goals_per_game).to eq(4.22)
     end
   end
+
+  describe '#average_goals_by_season' do
+    it "#average_goals_by_season" do
+      expected = {
+        "20122013"=>4.12,
+        "20162017"=>4.23,
+        "20142015"=>4.14,
+        "20152016"=>4.16,
+        "20132014"=>4.19,
+        "20172018"=>4.44
+      }
+      expect(@stat_calculator.average_goals_by_season).to eq(expected)
+    end
+  end
 end
  
