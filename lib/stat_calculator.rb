70b0a26539f146e4b0eb526b4b8c4c06ae73cefe
Dustin Peukert <dpeukert04@gmail.com>
2024-11-07 20:23:14 -0500

    write #lowest_scoring_visitor method

--- lib/stat_calculator.rb
+++ lib/stat_calculator.rb
@@ -35,4 +35,38 @@ class StatCalculator
   def count_of_teams
     @teams.count
   end
+
+  def lowest_scoring_visitor
+    all_scores = {}
+
+    @games.each do |game|
+      if all_scores[game.away_team_id].nil?
+        all_scores[game.away_team_id] = [game.away_goals]
+      else
+        all_scores[game.away_team_id] << game.away_goals
+      end
+    end
+    
+    all_average_scores = {}
+
+    all_scores.each do |team_id, scores|
+      num_of_scores = scores.count
+      sum_of_scores = scores.sum
+      average_score = sum_of_scores.to_f / scores.count.to_f
+
+      all_average_scores[team_id] = average_score.round(3)
+    end
+    
+    lowest_score = all_average_scores.min_by do |team_id, score|
+      score
+    end
+    
+    lowest_team_id = lowest_score[0].to_i
+    
+    team = @teams.find do |team|
+      team.team_id == lowest_team_id
+    end
+
+    team.team_name
+  end
 end
