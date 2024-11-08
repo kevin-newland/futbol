7c16f1fca266b0c35e901301c80c9f78c2cd61f6
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2024-11-08 01:38:19 +0100

    Implemented win percentage calculations in StatCalculator - Added methods: percentage_home_wins, percentage_visitor_wins, and percentage_ties to calculate respective percentages based on game data. Verified functionality with tests in stat_calculator_spec.rb.

--- lib/stat_calculator.rb
+++ lib/stat_calculator.rb
@@ -35,4 +35,25 @@ class StatCalculator
   def count_of_teams
     @teams.count
   end
+
+  # Calculates the percentage of games won by the home team
+  def percentage_home_wins
+    total_games = @games.size
+    home_wins = @games.count { |game| game.home_goals.to_i > game.away_goals.to_i }
+    ((home_wins.to_f / total_games) * 100).round(2)
+  end
+
+  # Calculates the percentage of games won by the visiting team
+  def percentage_visitor_wins
+    total_games = @games.size
+    visitor_wins = @games.count { |game| game.away_goals.to_i > game.home_goals.to_i }
+    ((visitor_wins.to_f / total_games) * 100).round(2)
+  end
+
+  # Calculates the percentage of games that ended in a tie
+  def percentage_ties
+    total_games = @games.size
+    ties = @games.count { |game| game.home_goals.to_i == game.away_goals.to_i }
+    ((ties.to_f / total_games) * 100).round(2)
+  end
 end
