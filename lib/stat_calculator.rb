2941ed05436c2915bdedef6525ebdc3bec34635f
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2024-11-08 01:03:14 +0100

    Added inline coments to StatCalculator Class

--- lib/stat_calculator.rb
+++ lib/stat_calculator.rb
@@ -6,18 +6,21 @@ class StatCalculator
     @game_teams = game_teams
   end
 
+  # @return [Float] - The percentage of games won by the home team, rounded to 2 decimal places.
   def percentage_home_wins
     total_games = @games.size
     home_wins = @games.count { |game| game.home_goals.to_i > game.away_goals.to_i }
     ((home_wins.to_f / total_games) * 100).round(2)
   end
 
+  # @return [Float] - The percentage of games won by the visiting team, rounded to 2 decimal places.
   def percentage_visitor_wins
     total_games = @games.size
     visitor_wins = @games.count { |game| game.away_goals.to_i > game.home_goals.to_i }
     ((visitor_wins.to_f / total_games) * 100).round(2)
   end
 
+  # @return [Float] - The percentage of tied games, rounded to 2 decimal places.
   def percentage_ties
     total_games = @games.size
     ties = @games.count { |game| game.home_goals.to_i == game.away_goals.to_i }
