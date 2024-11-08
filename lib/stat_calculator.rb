05a16126e18826f9ee828856b46473f525b13e64
Dustin Peukert <dpeukert04@gmail.com>
2024-11-07 20:29:02 -0500

    write #highest_scoring_visitor method

--- lib/stat_calculator.rb
+++ lib/stat_calculator.rb
@@ -61,4 +61,28 @@ class StatCalculator
 
     team.team_name
   end
+
+  def highest_scoring_visitor
+    all_scores = Hash.new { |hash, key| hash[key] = [] }
+
+    @games.each do |game|
+      all_scores[game.away_team_id] << game.away_goals
+    end
+    
+    all_average_scores = all_scores.transform_values do |scores|
+      (scores.sum.to_f / scores.size).round(3)
+    end
+    
+    highest_score = all_average_scores.max_by do |team_id, score|
+      score
+    end
+    
+    highest_team_id = highest_score[0].to_i
+    
+    team = @teams.find do |team|
+      team.team_id == highest_team_id
+    end
+
+    team.team_name
+  end
 end
